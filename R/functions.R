926e3ca2f7deeec0a59e33225ea668c448ff8c77
Morten Dall <dall@sund.ku.dk>
2020-10-05 16:52:37 +0200

    added RNAseq processing and made functions for the functions script

--- R/functions.R
+++ R/functions.R
@@ -72,3 +72,106 @@ ID_key_generator <- function(ID, Group){
 }
 
 
+#' Count_matrix_loader
+#'
+#' @param file - name of the count matrix file
+#'
+#' @return a count matrix tibble with gene IDs as row names
+
+count_matrix_load <- function(file) {
+  data_file <- fs::dir_ls(here::here("data-raw/"),
+                          regexp = file,
+                          recurse = T)
+  count_matrix <- vroom::vroom(data_file)
+  colnames(count_matrix) <- str_remove(colnames(count_matrix), "^0+")
+  count_matrix_key <- count_matrix$key
+  count_matrix <- count_matrix %>%
+    dplyr::select(-key)
+  rownames(count_matrix) = count_matrix_key
+  return(count_matrix)
+}
+
+#' Load metadata and sort them according to count matrix input
+#'
+#' @param file_name the name of teh metadata file (default "metadata.csv")
+#' @param count_matrix the name of the count matrix object
+#'
+#' @return metadata file sorted according to count matrix order
+
+load_metadata <- function(file_name, count_matrix) {
+  data_file <- fs::dir_ls(here::here("data-raw/"),
+                          regexp = file_name,
+                          recurse = T)
+  metadata <-
+    vroom::vroom(
+      data_file,
+      col_types = cols(
+        `gender:ch1` = col_character(),
+        `disease:ch1` = col_character(),
+        description = col_character()
+      )
+    )
+  data_order <- colnames(count_matrix)
+  data_order <- as_tibble(data_order)
+  colnames(data_order) = "description"
+  metadata <- full_join(data_order, metadata, "description")
+  return(metadata)
+}
+
+#' Generate design matrix
+#'
+#' @param metadata a metadata object generated through the load_metadata function
+#'
+#' @return a design matrix file
+
+Generate_design_matrix <- function(metadata){
+  group <- as.matrix(metadata[3])
+  design <- model.matrix( ~ 0 + group, metadata)
+  colnames(design) <-
+    str_remove_all(colnames(design), "\\(|\\)|group|:")
+  return(design)
+}
+
+#' RNAseq analysis
+#'
+#' @param count_matrix count matrix generated from count_matrix_load
+#' @param design design matrix made through the Generate_design_matrix
+#' @param metadata metadata generated from load_metadata function
+#' @param contrast_matrix generated through the limma::makeContrast function
+#'
+#' @return
+
+RNAseq_processing <- function(count_matrix, metadata, design, contrast_matrix) {
+  RNAseq <- DGEList(counts = count_matrix, group = group)
+  keep <- filterByExpr(RNAseq)
+  RNAseq <- RNAseq[keep, , keep.lib.sizes = F]
+  RNAseq <- calcNormFactors(RNAseq)
+  RNAseq <- estimateDisp(RNAseq,design, robust = T)
+  et <- exactTest(RNAseq)
+  efit <- glmQLFit(RNAseq, design, robust = T)
+  dgeResults <- apply(cont.matrix, 2, . %>%
+                        glmQLFTest(glmfit = efit, contrast = .) %>%
+                        topTags(n = Inf, p.value = 1) %>%
+                        extract2("table") %>%
+                        as.data.table(keep.rownames = TRUE))
+  return(dgeResults)
+}
+
+#' Annotate results
+#'
+#' @param Results_file an object containing ensembl IDs in the first column
+#'
+#' @return A list annotated with gene symbols
+
+annotated_dgeResults <- function(Results_file) {
+  dat <- data.table::copy(Results_file)
+  data.table::setnames(dat, names(dat)[1], "ENSEMBL")
+  ens2symbol <-
+    clusterProfiler::bitr(dat$ENSEMBL,
+         fromType = 'ENSEMBL',
+         toType = 'SYMBOL',
+         OrgDb = "org.Hs.eg.db")
+  ens2symbol %<>% data.table %>% data.table::setkey(ENSEMBL)
+  dat <- dplyr::full_join(dat, ens2symbol)
+  return(dat)
+}
--- data-raw/Suppli2019.R
+++ data-raw/Suppli2019.R
@@ -1,13 +1,28 @@
-## code to prepare `DATASET` dataset goes here
-
-GEO_data_file <- download_GEO_file("GSE126848")
+# # code to prepare `DATASET` dataset goes here
+#
+# GEO_data_file <- download_GEO_file("GSE126848")
 # #selects 3 characteristica, "gender:ch1", "disease:ch1" and "description"(which contains numerical ID)
 #
-metadata_file <- generate_metadata(c("gender:ch1", "disease:ch1", "description"))
-count_matrix <- count_matrix_assembly("txt")
+# metadata_file <- generate_metadata(c("gender:ch1", "disease:ch1", "description"))
+# count_matrix <- count_matrix_assembly("txt")
+#
+# # identified ID in count matrix as column "description" in metadata
+# ID_key_file <- ID_key_generator("description", "disease:ch1")
 
-# identified ID in count matrix as column "description" in metadata
-ID_key_file <- ID_key_generator("description", "disease:ch1")
+# Load in the count matrix
+count_matrix <- count_matrix_load("count_matrix.csv")
+metadata_test <- load_metadata("metadata.csv",count_matrix)
+
+design_matrix_test <- Generate_design_matrix(metadata_test)
+
+#use the design matrix to make a relevant contrast matrix
+cont.matrix <- limma::makeContrasts(
+  NAFLD_vs_healthy = NAFLD-healthy,
+  obese_vs_NAFLD = obese-NAFLD,
+  NASH_vs_NAFLD = NASH-NAFLD,
+  obese_vs_healthy = obese-healthy,
+  levels = design)
+
 
 
 
